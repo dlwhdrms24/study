9bb75b557edcc63ce1e189551407f5ebba5855f5
dlwhdrms24 <dlwhdrms24@gmail.com>
2025-09-10 12:13:38 +0900

    Update StringTokenTest.java

--- src/car/StringTokenTest.java
+++ src/car/StringTokenTest.java
@@ -5,15 +5,11 @@ import java.util.Date;
 
 public class StringTokenTest {
     public static void main(String[] args) {
-        // 1. 문제 02에서 만든 것과 동일한 형식의 문자열 준비
         Car car = new Car("그랜저", new Date(), "홍길동");
         String carInfo = car.toString();
 
-
-        // 2. 구분자로 사용할 문자들을 지정: " ,[]()="
         StringTokenizer st = new StringTokenizer(carInfo, " ,[]()=");
 
-        // 3. 토큰이 없을 때까지 반복하며 하나씩 출z력
         while (st.hasMoreTokens()) {
             System.out.println(st.nextToken());
         }
